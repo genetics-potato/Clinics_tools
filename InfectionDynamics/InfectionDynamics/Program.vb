c3b47c8e92ba13c822d7072653e7597a79bc4bef
xieguigang <xie.guigang@gmail.com>
2016-09-12 11:31:45 +0800

    r script

--- InfectionDynamics/InfectionDynamics/Program.vb
+++ InfectionDynamics/InfectionDynamics/Program.vb
@@ -4,11 +4,36 @@ Module Program
 
     Sub Main()
 
-        Call $"p <- {p}".ζ
+        Call $"p      <- {p}".ζ
+        Call $"beta   <- {beta}".ζ
+        Call $"c      <- {c}".ζ
+        Call $"delta  <- {delta}".ζ
+        Call $"U0     <- {U0}".ζ
+        Call $"rho    <- {rho}".ζ
+        Call $"lambda <- {lambda}".ζ
 
-        Dim fU As New ODEs.ODE With {.df = AddressOf dU, .SetY = Sub(x) U = x, .y0 = U0} ' The initial number of susceptible cells(U0) can be taken from the experiment in Half mannetal. (2008) as 5 × 10^5.
-        Dim fI As New ODEs.ODE With {.df = AddressOf dI, .SetY = Sub(x) I = x, .y0 = 0R}    ' The initial value for infected cells(I0) is set to zero.
-        Dim fV As New ODEs.ODE With {.df = AddressOf dV, .SetY = Sub(x) V = x, .y0 = 9}  ' The best model based on the Akaike Information Criterion(AIC) is presented in Figure3, providing an estimate of 9 ffu/ml for V0.
+        ' The initial value for infected cells(I0) is set to zero.
+        ' The best model based on the Akaike Information Criterion(AIC) is presented in Figure3, providing an estimate of 9 ffu/ml for V0.
+        ' The initial number of susceptible cells(U0) can be taken from the experiment in Half mannetal. (2008) as 5 × 10^5.
+        Dim yini = "c(U=U0, I=0, V=9)"
+        Dim model = [function](
+            {"t", "y", "params"},
+ _
+             "with(as.list(y), {
+    
+                dU <- lambda - rho * U - beta * U * V
+                dI <- beta * U * V - delta * I
+                dV <- p * I - c * V
+
+                list(c(dU, dI, dV))
+})")
+
+        Dim times = seq(from = 0, To = 5.6, by = 0.01)
+        out   <- ode(y = yini, times = times, func = Lorenz, parms = NULL)
+
+        Dim fU As New ODEs.ODE With {.df = AddressOf dU, .SetY = Sub(x) U = x, .y0 = U0}
+        Dim fI As New ODEs.ODE With {.df = AddressOf dI, .SetY = Sub(x) I = x, .y0 = 0R}
+        Dim fV As New ODEs.ODE With {.df = AddressOf dV, .SetY = Sub(x) V = x, .y0 = 9}
         Dim solver As New ODEs With {.ODEs = {fU, fI, fV}}
         Call solver.Solve(100, 0, 6)
     End Sub
