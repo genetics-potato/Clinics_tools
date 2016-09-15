349ec6c362775ca41c076a4d5a74732269b94065
xieguigang <xie.guigang@gmail.com>
2016-09-16 00:07:28 +0800

    add test example result

--- InfectionDynamics/InfectionDynamics/Program.vb
+++ InfectionDynamics/InfectionDynamics/Program.vb
@@ -7,20 +7,30 @@ Imports Microsoft.VisualBasic.Mathematical.Plots
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Mathematical.BasicR
+Imports Microsoft.VisualBasic.Mathematical.SyntaxAPI.MathExtension
 
 Module Program
 
     Sub Main()
 
-        Dim result = New Kinetics_of_influenza_A_virus_infection_in_humans().Solve(1000, 0, 8)
+        Dim result = New Kinetics_of_influenza_A_virus_infection_in_humans().Solve(10000, 0, 7)
 
         'result.y("V") = New NamedValue(Of Double()) With {
-        '    .Name = "V",
-        '    .x = -Log(New Vector(result.y("V").x))
+        '.Name = "V",
+        '.x = Log(New Vector(result.y("V").x), 2).ToArray
         '}
 
+        'result.y("I") = New NamedValue(Of Double()) With {
+        '.Name = "I",
+        '.x = VectorMath.Log(New Vector(result.y("I").x)).ToArray
+        ' }
+        'result.y("T") = New NamedValue(Of Double()) With {
+        '.Name = "T",
+        '.x = VectorMath.Log(New Vector(result.y("T").x)).ToArray
+        ' }
+
         Call result.DataFrame.Save("./Kinetics_of_influenza_A_virus_infection_in_humans.csv", Encodings.ASCII)
-        Call Scatter.Plot(result.FromODEs).SaveAs("./Kinetics_of_influenza_A_virus_infection_in_humans.png")
+        Call Scatter.Plot(result.FromODEs(, 10, 10)).SaveAs("./Kinetics_of_influenza_A_virus_infection_in_humans.png")
 
         Pause()
 
--- InfectionDynamics/Kinetics_of_influenza_A_virus_infection_in_humans/README.md
+++ InfectionDynamics/Kinetics_of_influenza_A_virus_infection_in_humans/README.md
@@ -1,4 +1,11 @@
-﻿Imports Microsoft.VisualBasic.Mathematical.BasicR
+Kinetics of influenza A virus infection in humans
+
+> **DOI:** 10.3390/v7102875
+
+###### ODEs Model
+
+```vbnet
+Imports Microsoft.VisualBasic.Mathematical.BasicR
 Imports Microsoft.VisualBasic.Mathematical.diffEq
 
 Public Class Kinetics_of_influenza_A_virus_infection_in_humans : Inherits ODEs
@@ -26,3 +33,12 @@ Public Class Kinetics_of_influenza_A_virus_infection_in_humans : Inherits ODEs
         }
     End Function
 End Class
+```
+
+##### Result
+
+###### Susceptible & Infected Cells
+![](./Cells.png)
+
+###### Virus
+![](./Virus.png)
