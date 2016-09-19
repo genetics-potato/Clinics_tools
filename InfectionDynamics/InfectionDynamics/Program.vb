0a401101fae515e314155bb00e51d6818a537e7d
xieguigang <xie.guigang@gmail.com>
2016-09-19 18:39:11 +0800

    fix random source

--- InfectionDynamics/InfectionDynamics/Program.vb
+++ InfectionDynamics/InfectionDynamics/Program.vb
@@ -1,42 +1,22 @@
-﻿Imports RDotNET.Extensions.VisualBasic
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Mathematical
+Imports Microsoft.VisualBasic.Mathematical.BasicR
+Imports Microsoft.VisualBasic.Mathematical.diffEq
+Imports Microsoft.VisualBasic.Mathematical.Plots
+Imports Microsoft.VisualBasic.Mathematical.SyntaxAPI.MathExtension
+Imports RDotNET.Extensions.Bioinformatics.deSolve
+Imports RDotNET.Extensions.Bioinformatics.deSolve.API
+Imports RDotNET.Extensions.VisualBasic
 Imports RDotNET.Extensions.VisualBasic.API.base
 Imports RDotNET.Extensions.VisualBasic.API.utils
-Imports RDotNET.Extensions.Bioinformatics.deSolve.API
-Imports RDotNET.Extensions.Bioinformatics.deSolve
-Imports Microsoft.VisualBasic.Mathematical.Plots
-Imports Microsoft.VisualBasic.Imaging
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
-Imports Microsoft.VisualBasic.Mathematical.BasicR
-Imports Microsoft.VisualBasic.Mathematical.SyntaxAPI.MathExtension
-Imports Microsoft.VisualBasic.ComponentModel.Ranges
-Imports Microsoft.VisualBasic.Mathematical.diffEq
-Imports Microsoft.VisualBasic.Language
 
 Module Program
 
     Sub Main()
-
-        ' bootstrapping test
-
-        Dim T As New NamedValue(Of DoubleRange) With {.Name = NameOf(T), .x = New DoubleRange(10 ^ 5, 10 ^ 10)}
-        Dim I As New NamedValue(Of DoubleRange) With {.Name = NameOf(I), .x = New DoubleRange(0, 10 ^ 3)}
-        Dim V As New NamedValue(Of DoubleRange) With {.Name = NameOf(V), .x = New DoubleRange(10 ^ -10, 1)}
-
-        Dim p As New NamedValue(Of DoubleRange) With {.Name = NameOf(p), .x = New DoubleRange(10 ^ -4, 10)}
-        Dim c As New NamedValue(Of DoubleRange) With {.Name = NameOf(c), .x = New DoubleRange(10 ^ -2, 10)}
-        Dim beta As New NamedValue(Of DoubleRange) With {.Name = NameOf(beta), .x = New DoubleRange(10 ^ -8, 1)}
-        Dim delta As New NamedValue(Of DoubleRange) With {.Name = NameOf(delta), .x = New DoubleRange(10 ^ -2, 10)}
-        Dim uid As New Uid
-
-        For Each x In BootstrapEstimate.Bootstrapping(Of Kinetics_of_influenza_A_virus_infection_in_humans)({p, c, beta, delta}, {T, I, V}, Long.MaxValue, 1000000, 0, 50)
-            Dim id As String = ++uid
-            Dim path As String = App.HOME & $"/be/{id.First}/{id}.csv"
-            Call x.DataFrame.Save(path, Encodings.ASCII)
-        Next
-
-        Pause()
-
-
 
         Dim result = New Kinetics_of_influenza_A_virus_infection_in_humans().Solve(10000, 0, 7)
 
@@ -55,6 +35,57 @@ Module Program
         ' }
 
         Call result.DataFrame.Save("./Kinetics_of_influenza_A_virus_infection_in_humans.csv", Encodings.ASCII)
+        'Dim rnd As New PreciseRandom(10 ^ -23, 300)
+
+        'Using ffdfdsf As StreamWriter = "x:\sdfsdfdsfs.txt".OpenWriter
+        '    For Isdfdsfs = 0 To 1000000
+        '        Call ffdfdsf.WriteLine(rnd.NextNumber)
+        '    Next
+        'End Using
+        'Pause()
+        ' bootstrapping test
+
+        Dim T As New NamedValue(Of PreciseRandom) With {.Name = NameOf(T), .x = New PreciseRandom(10 ^ 5, 10 ^ 9)}
+        Dim I As New NamedValue(Of PreciseRandom) With {.Name = NameOf(I), .x = New PreciseRandom(0, 10 ^ 3)}
+        Dim V As New NamedValue(Of PreciseRandom) With {.Name = NameOf(V), .x = New PreciseRandom(10 ^ -5, 1)}
+
+        For Iddddddd = 0 To 100
+            Call I.x.NextNumber.__DEBUG_ECHO
+        Next
+
+        Dim p As New NamedValue(Of PreciseRandom) With {.Name = NameOf(p), .x = New PreciseRandom(10 ^ -4, 1)}
+        Dim c As New NamedValue(Of PreciseRandom) With {.Name = NameOf(c), .x = New PreciseRandom(10 ^ -2, 10)}
+        Dim beta As New NamedValue(Of PreciseRandom) With {.Name = NameOf(beta), .x = New PreciseRandom(10 ^ -10, 1)}
+        Dim delta As New NamedValue(Of PreciseRandom) With {.Name = NameOf(delta), .x = New PreciseRandom(10 ^ -2, 15)}
+        Dim uid As New Uid
+
+        For Each x In BootstrapEstimate.Bootstrapping(Of Kinetics_of_influenza_A_virus_infection_in_humans)({p, c, beta, delta}, {T, I, V}, Long.MaxValue, 10000, 0, 30)
+            Dim id As String = ++uid
+            Dim path As String = App.HOME & $"/be/{id.First}/{id}.csv"
+            Call x.DataFrame.Save(path, Encodings.ASCII)
+        Next
+
+        Pause()
+
+
+
+        '  Dim result = New Kinetics_of_influenza_A_virus_infection_in_humans().Solve(10000, 0, 7)
+
+        'result.y("V") = New NamedValue(Of Double()) With {
+        '.Name = "V",
+        '.x = Log(New Vector(result.y("V").x), 2).ToArray
+        '}
+
+        'result.y("I") = New NamedValue(Of Double()) With {
+        '.Name = "I",
+        '.x = VectorMath.Log(New Vector(result.y("I").x)).ToArray
+        ' }
+        'result.y("T") = New NamedValue(Of Double()) With {
+        '.Name = "T",
+        '.x = VectorMath.Log(New Vector(result.y("T").x)).ToArray
+        ' }
+
+        '   Call result.DataFrame.Save("./Kinetics_of_influenza_A_virus_infection_in_humans.csv", Encodings.ASCII)
         Call Scatter.Plot(result.FromODEs(, 10, 10)).SaveAs("./Kinetics_of_influenza_A_virus_infection_in_humans.png")
 
 
