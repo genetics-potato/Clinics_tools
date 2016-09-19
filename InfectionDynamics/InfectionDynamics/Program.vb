0380419dd9038b547823c44217f8835f67cfe603
xieguigang <xie.guigang@gmail.com>
2016-09-19 12:31:06 +0800

    bootstrapping test

--- InfectionDynamics/InfectionDynamics/Program.vb
+++ InfectionDynamics/InfectionDynamics/Program.vb
@@ -8,10 +8,35 @@ Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Mathematical.BasicR
 Imports Microsoft.VisualBasic.Mathematical.SyntaxAPI.MathExtension
+Imports Microsoft.VisualBasic.ComponentModel.Ranges
+Imports Microsoft.VisualBasic.Mathematical.diffEq
+Imports Microsoft.VisualBasic.Language
 
 Module Program
 
     Sub Main()
+
+        ' bootstrapping test
+
+        Dim T As New NamedValue(Of DoubleRange) With {.Name = NameOf(T), .x = New DoubleRange(10 ^ 5, 10 ^ 10)}
+        Dim I As New NamedValue(Of DoubleRange) With {.Name = NameOf(I), .x = New DoubleRange(0, 10 ^ 3)}
+        Dim V As New NamedValue(Of DoubleRange) With {.Name = NameOf(V), .x = New DoubleRange(10 ^ -10, 1)}
+
+        Dim p As New NamedValue(Of DoubleRange) With {.Name = NameOf(p), .x = New DoubleRange(10 ^ -4, 10)}
+        Dim c As New NamedValue(Of DoubleRange) With {.Name = NameOf(c), .x = New DoubleRange(10 ^ -2, 10)}
+        Dim beta As New NamedValue(Of DoubleRange) With {.Name = NameOf(beta), .x = New DoubleRange(10 ^ -8, 1)}
+        Dim delta As New NamedValue(Of DoubleRange) With {.Name = NameOf(delta), .x = New DoubleRange(10 ^ -2, 10)}
+        Dim uid As New Uid
+
+        For Each x In BootstrapEstimate.Bootstrapping(Of Kinetics_of_influenza_A_virus_infection_in_humans)({p, c, beta, delta}, {T, I, V}, Integer.MaxValue, 1000000, 0, 50)
+            Dim id As String = ++uid
+            Dim path As String = App.HOME & $"/be/{id.First}/{id}.csv"
+            Call x.DataFrame.Save(path, Encodings.ASCII)
+        Next
+
+        Pause()
+
+
 
         Dim result = New Kinetics_of_influenza_A_virus_infection_in_humans().Solve(10000, 0, 7)
 
