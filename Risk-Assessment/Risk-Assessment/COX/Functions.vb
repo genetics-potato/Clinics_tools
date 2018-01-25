45717a04439cf6a0b837d2cb2425a1f82dbe90b4
xieguigang <xie.guigang@gcmodeller.org>
2018-01-25 18:26:54 +0800

    add test

---
+++ Risk-Assessment/Risk-Assessment/COX/Functions.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Data.Bootstrapping
+Imports Microsoft.VisualBasic.Math
+
+Namespace COX
+
+    Public Module Functions
+
+        ''' <summary>
+        ''' 生存函数：
+        ''' 
+        ''' ```
+        ''' S(t) = t时刻仍存活的人数/观察的总人数
+        ''' ```
+        ''' </summary>
+        ''' <param name="model"></param>
+        ''' <returns></returns>
+        <Extension>
+        Public Function SurvivalModel(model As IEnumerable(Of Model)) As FitResult
+            Dim timeGroups = model.Select(Function(m) m.time).GroupBy(offsets:=0.1)
+
+        End Function
+    End Module
+End Namespace
--- Risk-Assessment/test/Module1.vb
+++ Risk-Assessment/test/Module1.vb
@@ -1,7 +1,17 @@
-﻿Module Module1
+﻿Imports Microsoft.VisualBasic.Data.Bootstrapping
+Imports SMRUCC.Clinic.RiskAssessment.COX
+
+Module Module1
 
     Sub Main()
-
+        Call survialTest()
     End Sub
 
+    Sub survialTest()
+        Dim modelData = Model.LoadDataSet(Of Model)("D:\Clinics_tools\Risk-Assessment\lung.csv", "inst").ToArray
+        Dim S As FitResult = modelData.SurvivalModel
+
+
+        Pause()
+    End Sub
 End Module
