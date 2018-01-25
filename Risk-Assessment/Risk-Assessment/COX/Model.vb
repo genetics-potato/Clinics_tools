e5278d34992b13de3c2bf45bbc257a5f5a716cf6
xieguigang <xie.guigang@gcmodeller.org>
2018-01-25 10:45:15 +0800

    fix for namespace

--- Risk-Assessment/Risk-Assessment/COX/Model.vb
+++ Risk-Assessment/Risk-Assessment/COX/Model.vb
@@ -1,29 +1,32 @@
 ﻿Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Data.csv.StorageProvider.Reflection
 
-Public Class Model : Inherits DataSet
+Namespace COX
 
-    Public Property time As Double
-    <Column("status", GetType(COX.statusCode))>
-    Public Property status As COXstatus
+    Public Class Model : Inherits DataSet
 
-    Public Overrides Function ToString() As String
-        Return $"[{ID}] {time}, {status.Description}"
-    End Function
-End Class
+        Public Property time As Double
+        <Column("status", GetType(Coefficients.statusCode))>
+        Public Property status As COXstatus
 
-''' <summary>
-''' + 1 是正常无患病的截尾数据
-''' + 2 表示患病了
-''' </summary>
-Public Enum COXstatus As Integer
+        Public Overrides Function ToString() As String
+            Return $"[{ID}] {time}, {status.Description}"
+        End Function
+    End Class
 
     ''' <summary>
-    ''' 没有发生死亡事件的截尾数据
+    ''' + 1 是正常无患病的截尾数据
+    ''' + 2 表示患病了
     ''' </summary>
-    Censored = 1
-    ''' <summary>
-    ''' 发生了死亡事件
-    ''' </summary>
-    Die = 2
-End Enum
+    Public Enum COXstatus As Integer
+
+        ''' <summary>
+        ''' 没有发生死亡事件的截尾数据
+        ''' </summary>
+        Censored = 1
+        ''' <summary>
+        ''' 发生了死亡事件
+        ''' </summary>
+        Die = 2
+    End Enum
+End Namespace
