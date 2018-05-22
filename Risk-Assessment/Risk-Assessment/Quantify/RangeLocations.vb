4cac75b4a77ea05b08e08e779ea062c0b58b5959
xieguigang <xie.guigang@gcmodeller.org>
2018-05-22 15:09:01 +0800

    range locations of a metabolite

---
+++ Risk-Assessment/Risk-Assessment/Quantify/RangeLocations.vb
@@ -0,0 +1,9 @@
+﻿Imports System.ComponentModel
+
+Public Enum RangeLocations
+    <Description("red")> LowRed
+    <Description("yellow")> LowYellow
+    <Description("green")> Green
+    <Description("yellow")> HiYellow
+    <Description("red")> HiRed
+End Enum
